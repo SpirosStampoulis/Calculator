77402cdbe3ec0bc809554f810faa7c89aea5bffa
Spiros Stampoulis <sstaboulis@gmail.com>
2021-02-20 17:45:21 +0100

    add and del

---
+++ src/scripts/reducers/add.js
@@ -0,0 +1,64 @@
+import { ADD } from '../actions/constants';
+import helper from '../model/helper';
+
+function _concatValues(calculated, state, lastCommand, value) {
+  return calculated || helper.isNumberZero(state) || helper.isNumberZero(state + value) || (helper.isEmpty(lastCommand) && calculated);
+}
+
+function _getLastCommand(commands) {
+  let lastCommand = [];
+
+  if (commands.length > 1) {
+    lastCommand = commands.pop();
+  }
+
+  return lastCommand;
+}
+
+function _getCommands(historyDisplay) {
+  let commands = [];
+
+  if (historyDisplay) {
+    commands = historyDisplay.split('');
+  }
+
+  return commands;
+}
+
+function _appendValues({ output, calculated, state, lastCommand, value }) {
+  if (_concatValues(calculated, state, lastCommand, value)) {
+    output = value;
+  } else {
+    output += `${state}${value}`;
+  }
+
+  return output;
+}
+
+function _getOutput(conditional, firstResult, secondResult) {
+  return conditional ? firstResult : secondResult;
+}
+
+const maxDisplay = 15;
+
+function add(state = '', action) {
+  let output = '';
+  let lastCommand = [];
+  const { historyDisplay, displayValue, calculated } = action.data;
+
+  switch (action.type) {
+    case ADD:
+      lastCommand = _getLastCommand(_getCommands(historyDisplay));
+
+      output = helper.hasValue(state) ? _appendValues({ output, calculated, state, lastCommand, value: action.value }) : output;
+
+      output = _getOutput(output.length, output, action.value);
+
+      output = _getOutput(output.length > maxDisplay, displayValue, output);
+
+      return output;
+  }
+  return state;
+}
+
+export default add;
---
+++ src/scripts/reducers/del.js
@@ -0,0 +1,20 @@
+import { DEL } from '../actions/constants';
+import helper from '../model/helper';
+
+function del(state = '0', action) {
+  let output = '0';
+
+  switch (action.type) {
+    case DEL:
+      output = helper.removeLastChar(state);
+      if (helper.isNaN(output) || helper.isNumberZero(output)) {
+        output = '0';
+      }
+
+      return output;
+  }
+
+  return state;
+}
+
+export default del;
